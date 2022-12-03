1804cbbcfb6ee5ddba15f38740326f367bea6028
Yash2310786 <113800676+Yash2310786@users.noreply.github.com>
2022-12-03 17:30:16 -0500

    Employee and Employee dir created

---
+++ AED_FINAL_GROUP_PROJECT/src/Business/Employee/Employee.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Employee;
+
+/**
+ *
+ * @author yashrevadekar
+ */
+public class Employee {
+    private String name;
+    private int id;
+    private static int count=1;
+    // constructor autogenerates employee id 
+    public Employee(){
+        id=count;
+        count++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString(){
+        return name;
+    }
+}
