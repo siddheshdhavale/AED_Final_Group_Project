5afc278ea256e3d9ca70f204da2c0d3b467cbd8d
Siddhesh Dhavale <dhavale.s@northeastern.edu>
2022-12-06 15:58:24 -0500

    Business.TaskQueue Package Classes created and added

--- AED_FINAL_GROUP_PROJECT/src/Business/TaskQueue/TaskRequest.java
+++ AED_FINAL_GROUP_PROJECT/src/Business/TaskQueue/TaskRequest.java
@@ -4,10 +4,150 @@
  */
 package Business.TaskQueue;
 
+import Business.CSO.CSO;
+import Business.Cops.Cops;
+import Business.EventCreator.EventCreator;
+import Business.FireFighter.FireFighter;
+import Business.UserCredentials.UserCredentials;
+import Business.Victim.Victim;
+import Business.Volunteer.Volunteer;
+import java.util.Date;
+import Business.Supplier.Item;
+import Business.Supplier.Supplier;
+
 /**
  *
  * @author siddh
  */
 public class TaskRequest {
+    
+    private String subject;
+    private UserCredentials sender;
+    private UserCredentials reciever;
+    private String status;
+    private Date requestDate;
+    private Date resolveDate;
+    private EventCreator eventCreator;
+    private Victim changeSeeker;
+    private CSO cso;
+    private Supplier supplier;
+    private Item item;
+    private Volunteer volunteer;
+    private Cops cops;
+    private FireFighter firefighter;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public UserCredentials getSender() {
+        return sender;
+    }
+
+    public void setSender(UserCredentials sender) {
+        this.sender = sender;
+    }
+
+    public UserCredentials getReciever() {
+        return reciever;
+    }
+
+    public void setReciever(UserCredentials reciever) {
+        this.reciever = reciever;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public Date getResolveDate() {
+        return resolveDate;
+    }
+
+    public void setResolveDate(Date resolveDate) {
+        this.resolveDate = resolveDate;
+    }
+
+    public EventCreator getEventCreator() {
+        return eventCreator;
+    }
+
+    public void setEventCreator(EventCreator eventCreator) {
+        this.eventCreator = eventCreator;
+    }
+
+    public Victim getChangeSeeker() {
+        return changeSeeker;
+    }
+
+    public void setChangeSeeker(Victim changeSeeker) {
+        this.changeSeeker = changeSeeker;
+    }
+
+    public CSO getCso() {
+        return cso;
+    }
+
+    public void setCso(CSO cso) {
+        this.cso = cso;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public Volunteer getVolunteer() {
+        return volunteer;
+    }
+
+    public void setVolunteer(Volunteer volunteer) {
+        this.volunteer = volunteer;
+    }
+
+    public Cops getCops() {
+        return cops;
+    }
+
+    public void setCops(Cops cops) {
+        this.cops = cops;
+    }
+
+    public FireFighter getFirefighter() {
+        return firefighter;
+    }
+
+    public void setFirefighter(FireFighter firefighter) {
+        this.firefighter = firefighter;
+    }
+    
+   
     
 }
--- AED_FINAL_GROUP_PROJECT/src/Business/TaskQueue/VictimTaskRequest.java
+++ AED_FINAL_GROUP_PROJECT/src/Business/TaskQueue/VictimTaskRequest.java
@@ -8,6 +8,39 @@ package Business.TaskQueue;
  *
  * @author siddh
  */
-public class VictimTaskRequest {
+public class VictimTaskRequest extends TaskRequest{
+    private String Description;
+    private String Location;
+    private boolean add;
+    
+    public VictimTaskRequest() {
+        
+        add = false;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String Location) {
+        this.Location = Location;
+    }
+
+    public boolean isAdd() {
+        return add;
+    }
+
+    public void setAdd(boolean add) {
+        this.add = add;
+    }
+    
     
 }
