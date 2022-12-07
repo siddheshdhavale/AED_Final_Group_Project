b69ea63e62ddec244e5449a2c6fb0fd052398359
darshan nemade <nemaded.nemade.d@northeastern.edu>
2022-12-06 19:44:46 -0500

    Role,supplieradminrole added

--- AED_FINAL_GROUP_PROJECT/src/Business/Role/Role.java
+++ AED_FINAL_GROUP_PROJECT/src/Business/Role/Role.java
@@ -4,12 +4,59 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Business.Role;
+import Business.Ecosystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserCredentials.UserCredentials;
+import javax.swing.JPanel;
 
 /**
  *
  * @author siddh
  */
-public class Role {
+public abstract class Role {
+    //enums for types of roles
+    public enum RoleType{
+        SysAdmin("SysAdmin"),
+        VictimAdmin("VictimAdmin"),
+        Victim("Victim"),
+        EventCreatorAdmin("EventCreatorAdmin"),
+        EventCreator("EventCreator"),
+        EnterPriseAdmin("EnterpriseAdmin"),
+        CommAdminRole("CommAdminRole"),
+        CSOAdminRole("CSOAdminRole"),
+        CSOManagerRole("CSOManagerRole"),
+        SupplierAdminRole("SupplierAdminRole"),
+        SupplierManagerRole("SupplierManagerRole"),
+        VolunteerAdminRole("VolunteerAdminRole"),
+        VolunteerRole("VolunteerRole"),
+        SupplierRole("SupplierRole"),
+        CopsRole("CopsRole"),
+        CopsAdminRole("CopsAdminRole"),
+        DoctorAdminRole("DoctorAdminRole"),
+        Doctor("Doctor"),
+        FireFighterRole("FireFighterRole"),
+        FireFighterAdminRole("FireFighterAdminRole");
+        
+        
+        
+        private String value;
+        private RoleType(String value){
+            this.value=value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+        
+        @Override
+        public String toString(){
+            return value;
+        }
+    }
+    
+    public abstract JPanel createWorkArea(JPanel userProcessContainer,UserCredentials credentials,Organization organization,Enterprise enterprise,Ecosystem system);
+    
+    
     
 }
-
--- AED_FINAL_GROUP_PROJECT/src/Business/Role/SupplierAdminRole.java
+++ AED_FINAL_GROUP_PROJECT/src/Business/Role/SupplierAdminRole.java
@@ -4,12 +4,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Business.Role;
-
+import Business.Ecosystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserCredentials.UserCredentials;
+import UI.SysAdmin.SysAdminWorkArea;
+import javax.swing.JPanel;
 /**
  *
  * @author siddh
  */
 public class SupplierAdminRole extends Role{
+   
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer,UserCredentials credentials,Organization organization,Enterprise enterprise,Ecosystem system){
+       return new SysAdminWorkArea(userProcessContainer,system);
+    }
+        @Override
+        public String toString()
+{
+   return Role.RoleType.SysAdmin.getValue();
+}
     
 }
 
